c35b1dc4df7f2f63b1d02824517f35b058083fd7
Elephant Liu <liuxiaojiang@douban.com>
2025-04-07 18:01:00 +0800

    add test for prefetch when get multi in different order

--- tests/test_field.py
+++ tests/test_field.py
@@ -397,6 +397,28 @@ class TestDbField(TestCase):
                 self.assertEqual(db_get.call_count, 0)
                 self.assertEqual(db_get_multi.call_count, 4)
 
+    def test_prefetch_in_get_multi(self):
+        for i in xrange(1, 8):
+            Dummy.create(count=i, prop1=[i])
+
+        # get multi in different order
+        for ids in [
+            [1, 2, 3],
+            [3, 2, 1],
+            [2, 3, 1],
+        ]:
+            with patched_db_get as db_get:
+                with patched_db_get_multi as db_get_multi:
+                    print('ids:', ids)
+                    ds = Dummy.get_multi(ids)
+                    print('entities ids:', [d.id for d in ds[0]._olo_qs.entities])
+
+                    for d in ds:
+                        self.assertEqual(d.count, d.id)
+
+                    self.assertEqual(db_get.call_count, 0)
+                    self.assertEqual(db_get_multi.call_count, 3)
+
 
 class TestBatchField(TestCase):
 
