38b8fb6b84e41a3458eff428efed55e40dab5a1d
Elephant Liu <liuxiaojiang@douban.com>
2025-04-07 18:35:18 +0800

    remove print

--- tests/test_field.py
+++ tests/test_field.py
@@ -410,15 +410,11 @@ class TestDbField(TestCase):
             with self.subTest(ids=ids):
                 with patched_db_get as db_get:
                     with patched_db_get_multi as db_get_multi:
-                        print('ids:', ids)
                         ds = Dummy.get_multi(ids)
-                        print('entities ids:', [d.id for d in ds[0]._olo_qs.entities])
 
                         for d in ds:
                             self.assertEqual(d.count, d.id)
 
-                        print('db get call count:', db_get.call_count)
-                        print('db get multi call count:', db_get_multi.call_count)
                         self.assertEqual(db_get.call_count, 0)
                         self.assertEqual(db_get_multi.call_count, 2)
 
